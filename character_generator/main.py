8849e9b2bd9b8401df099a950e79230400b91470
Dmitriy Firsov <shtihlfd@yandex.ru>
2023-09-29 20:46:42 +0300

    Create main.py for lesson 5 "Character generator"

---
+++ character_generator/main.py
@@ -0,0 +1,76 @@
+import file_operations
+from random import sample, randint
+from faker import Faker
+
+
+def main():
+    fake = Faker("ru_RU")
+    letters_dict = {
+        'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+        'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+        'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+        'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+        'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+        'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+        'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+        'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+        'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+        'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+        'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+        'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+        'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+        'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+        'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+        'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+        'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+        'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+        'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+        'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+        'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+        'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+        ' ': ' '
+    }
+    skills = [
+        'Стремительный прыжок',
+        'Электрический выстрел',
+        'Ледяной удар',
+        'Стремительный удар',
+        'Кислотный взгляд',
+        'Тайный побег',
+        'Ледяной выстрел',
+        'Огненный заряд'
+    ]
+    runic_skills = []
+    for skill in skills:
+        for letter in skill:
+            skill = skill.replace(letter, letters_dict[letter])
+        runic_skills.append(skill)
+
+    
+    card_quantity = 10
+    
+    for card in range(card_quantity):
+        choice_skills = sample(runic_skills, 3)
+        context = {
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
+            "town": fake.city(),
+            "job": fake.job(),
+            "strength": randint(3, 18),
+            "agility": randint(3, 18),
+            "endurance": randint(3, 18),
+            "intelligence": randint(3, 18),
+            "luck": randint(3, 18),
+            "skill_1": choice_skills[0],
+            "skill_2": choice_skills[1],
+            "skill_3": choice_skills[2]
+        }
+        file_operations.render_template(
+            f"src/charsheet.svg",
+            f"out/card_{card}.svg",
+            context
+        )
+
+
+if __name__ == '__main__':
+    main()import file_operations
